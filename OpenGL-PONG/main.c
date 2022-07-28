6300acf9bf2cb13f5706776a1928d5a410cceb6d
eevaain <evanlin416@gmail.com>
2022-07-28 11:13:09 -0400

    fixed dependencies

---
+++ OpenGL-PONG/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <SDL.h>
+
+
+
+int main(int argc, char* args[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	printf("Hello world");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// in old systems, rendering pixels was easy -> you "poke" pixels into memory
+// addresses
