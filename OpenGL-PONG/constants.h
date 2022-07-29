e897c99fc49e2f6d0a8804626addf0d1efc7efa1
eevaain <evanlin416@gmail.com>
2022-07-28 22:52:59 -0400

    lets goooo

--- OpenGL-PONG/constants.h
+++ OpenGL-PONG/constants.h
@@ -3,3 +3,8 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+
+#define FPS 30 // we only need this for the next line :)
+#define FRAME_TARGET_TIME (1000/FPS) // duration of each frame per second
+// 1000 milliseconds (because we count in milliseconds) 
+// therefore each frame will last 33.3 milliseconds on screen 
--- OpenGL-PONG/main.c
+++ OpenGL-PONG/main.c
@@ -23,6 +23,15 @@ int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL; 
 
+int last_frame_time = 0; // milliseconds of the last frame called
+
+struct ball {
+	float x;
+	float y;
+	float width;
+	float height;
+} ball; //already declare ball
+
 // whenever a function doesnt recieve anything, speficy void in parameters
 int initialize_window(void) {
 	// initializes everything cus we're lazy
@@ -47,13 +56,14 @@ int initialize_window(void) {
 	}
 	// -1 means default display driver, 0 means no special way of rendering
 	renderer = SDL_CreateRenderer(window, -1, 0);
-		if (!renderer) {
-			fprintf(stderr, "Error creating an SDL renderer.\n");
-			return FALSE;
-		}
-
+	if (!renderer) {
+		fprintf(stderr, "Error creating an SDL renderer.\n");
+		return FALSE;
+	}
 	return TRUE;
 }
+
+
 
 void process_input() {
 	SDL_Event event;
@@ -70,15 +80,60 @@ void process_input() {
 	}
 }
 
-void update() {
 
+
+void setup() {
+	ball.x = 20;
+	ball.y = 20;
+	ball.width = 15;
+	ball.height = 15;
+}
+
+
+void update() {
+	// WASTE time/sleep until we reach frame target time
+	Uint32 timeout = last_frame_time + FRAME_TARGET_TIME;
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), timeout)) {
+		/* ... do work until timeout has elapsed */
+	}
+
+	last_frame_time = SDL_GetTicks();
+
+	ball.x += 2;
+	ball.y += 2;
 }
 
 void render() {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	//draw rec (SDL_Rect is a type from SDL) 
+	SDL_Rect ball_rect = {
+		(int)ball.x, // (int) is called a "cast" -> casting float to int
+		(int)ball.y,
+		(int)ball.width,
+		(int)ball.height
+	};
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	// we pass in the address of the struct
+	// instead of passing all the values of the struct one by one,
+	// we just pass in a pointer of where the object is located
+	// by doing this, we AVOID copying values in memory. 
+	SDL_RenderFillRect(renderer, &ball_rect);
+
+	SDL_RenderPresent(renderer); 
+
+	// here is where we an start drawing our game objs
+	// SDL use double buffering 
+	//we see front buffer -> back buffer populating.. when its done
+	// buffers "swap"
+	// swaps "virtual" screen buffers
+	SDL_RenderPresent(renderer);
 
 }
 
-void destory_window() {
+void destroy_window() {
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -86,17 +141,19 @@ void destory_window() {
 
 
 int main() {
-	int game_is_running = initialize_window();
+	int game_is_running = initialize_window(); //#1 initialize window
 
-	setup();
+	setup(); // #2 variables of game
 
-	while (game_is_running) {
-		process_input();
+	while (game_is_running) { // #3 game loop 
+
+		process_input(); //#3.1 process keyboard input into game state changes
 		update();
-		render(); //draw 
+		render(); //clears screen, draws pixels, double buffer
+
 	}
 
-	destroy_window();
+	destroy_window(); // when game_is_running is false this runs!
 
 	return 0;
 }
